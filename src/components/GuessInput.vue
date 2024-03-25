a0884ad53a46b56cd95adfbbd53530db28cbf13f
anamboijohn <anamboijohn@gmail.com>
2024-03-25 10:47:01 +0000

    extracted guess input into its own component

---
+++ src/components/GuessInput.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { WORD_SIZE } from '@/settings'
+import { ref, computed } from 'vue'
+import englishWords from '@/englishWordsWith5Letters.json'
+
+const emit = defineEmits<{
+  'guess-submitted': [guess: string]
+}>()
+
+const word = ref<string | null>(null)
+
+const formattedWord = computed<string>({
+  get: () => word.value ?? '',
+  set: (value: string) => {
+    word.value = null
+    word.value = value
+      .slice(0, WORD_SIZE)
+      .toUpperCase()
+      .replace(/[^A-Z]+/gi, '')
+  }
+})
+
+const onSubmit = () => {
+  if (!englishWords.includes(formattedWord.value)) {
+    return
+  }
+ emit('guess-submitted', formattedWord.value)
+}
+
+const onKeyDown = (event: KeyboardEvent) => {
+  const char = event.key.toUpperCase()
+  if (
+    (!/^[A-Z]$/.test(char) || formattedWord.value.length >= WORD_SIZE) &&
+    char !== 'BACKSPACE' &&
+    char !== 'DELETE'
+  ) {
+    event.preventDefault()
+  }
+}
+</script>
+
+<template>
+  <input
+    type="text"
+    name="word"
+    id="word"
+    v-model="formattedWord"
+    @keydown.enter="onSubmit"
+    @keydown="onKeyDown"
+  />
+</template>
--- src/components/WordleBoard.vue
+++ src/components/WordleBoard.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-import { VICTORY_MSG, DEFEAT_MSG, WORD_SIZE } from '@/settings'
-import { ref, computed } from 'vue'
+import { VICTORY_MSG, DEFEAT_MSG } from '@/settings'
 import englishWords from '@/englishWordsWith5Letters.json'
+import GuessInput from './GuessInput.vue'
+import { ref } from 'vue'
 defineProps({
   wordOfTheDay: {
     type: String,
@@ -9,47 +10,13 @@ defineProps({
   }
 })
 
-const word = ref<string | null>(null)
-const submitted = ref(false)
-
-const formattedWord = computed<string>({
-  get: () => word.value ?? '',
-  set: (value: string) => {
-    word.value = null
-    word.value = value
-      .slice(0, WORD_SIZE)
-      .toUpperCase()
-      .replace(/[^A-Z]+/gi, '')
-  }
-})
-
-const onSubmit = () => {
-  if (!englishWords.includes(formattedWord.value)) {
-    return
-  }
-  submitted.value = true
-}
-
-const onKeyDown = (event: KeyboardEvent) => {
-  const char = event.key.toUpperCase()
-  if (
-    (!/^[A-Z]$/.test(char) || formattedWord.value.length >= WORD_SIZE) &&
-    char !== 'BACKSPACE' &&
-    char !== 'DELETE'
-  ) {
-    event.preventDefault()
-  }
-}
+const guessSubmitted = ref('')
 </script>
 
 <template>
-  <input
-    type="text"
-    name="word"
-    id="word"
-    v-model="formattedWord"
-    @keydown.enter="onSubmit"
-    @keydown="onKeyDown"
-  />
-  <p v-if="submitted" v-text="word == wordOfTheDay ? VICTORY_MSG : DEFEAT_MSG"></p>
+  <guess-input @guess-submitted="(guess) => (guessSubmitted = guess)" />
+  <p
+    v-if="guessSubmitted.length > 0"
+    v-text="guessSubmitted == wordOfTheDay ? VICTORY_MSG : DEFEAT_MSG"
+  ></p>
 </template>
