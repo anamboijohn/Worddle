de4f6cc9cc67cecd2aeab03a333133f6071f2569
anamboijohn <anamboijohn@gmail.com>
2024-03-25 13:04:01 +0000

    reduce repition in test

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -24,24 +24,19 @@ describe('WordleBoard', () => {
     })
 
     describe.each(
-      [
-        { numberOfGuesses: 0, shouldSeeDefeatMessage: false },
-        { numberOfGuesses: 1, shouldSeeDefeatMessage: false },
-        { numberOfGuesses: 3, shouldSeeDefeatMessage: false },
-        { numberOfGuesses: 4, shouldSeeDefeatMessage: false },
-        { numberOfGuesses: 5, shouldSeeDefeatMessage: false },
-        { numberOfGuesses: MAX_GUESSES_COUNT, shouldSeeDefeatMessage: true }
-      ],
-      { context: true }
+      Array.from({ length: MAX_GUESSES_COUNT + 1 }, (_, numberOfGuesses) => ({
+        numberOfGuesses,
+        shouldSeeTheDefeatMessage: numberOfGuesses === MAX_GUESSES_COUNT
+      }))
     )(
-      'a defeat message should appear if the player makes incorrect guesses 6 times in a row',
-      ({ numberOfGuesses, shouldSeeDefeatMessage }) => {
-        test('therefore for $numberOfGuesses guess(es)', async () => {
+      `a defeat message should appear if the player makes incorrect guesses ${MAX_GUESSES_COUNT} times`,
+      ({ numberOfGuesses, shouldSeeTheDefeatMessage }) => {
+        test(`therefore, for ${numberOfGuesses} guess(es) a defeat message should ${shouldSeeTheDefeatMessage ? '' : 'not'} appear`, async () => {
           for (let i = 0; i < numberOfGuesses; i++) {
             await playerSubmitsGuess('WRONG')
           }
 
-          if (shouldSeeDefeatMessage) {
+          if (shouldSeeTheDefeatMessage) {
             expect(wrapper.text()).toContain(DEFEAT_MSG)
           } else {
             expect(wrapper.text()).not.toContain(DEFEAT_MSG)
