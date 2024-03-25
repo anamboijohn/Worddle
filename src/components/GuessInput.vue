484377297012393786a2f244a304b5f65beb171e
anamboijohn <anamboijohn@gmail.com>
2024-03-25 11:04:51 +0000

    beautified guess input

--- src/components/GuessInput.vue
+++ src/components/GuessInput.vue
@@ -40,6 +40,16 @@ const onKeyDown = (event: KeyboardEvent) => {
 </script>
 
 <template>
+  <ul class="word">
+    <li
+      v-for="(letter, index) in formattedWord.padEnd(WORD_SIZE, ' ')"
+      :key="`${letter}-${index}`"
+      :data-letter="letter"
+      class="letter"
+      v-text="letter"
+    />
+  </ul>
+
   <input
     type="text"
     name="word"
@@ -48,8 +58,42 @@ const onKeyDown = (event: KeyboardEvent) => {
     v-model="formattedWord"
     @keydown.enter="onSubmit"
     @keydown="onKeyDown"
-
     autofocus
-    @blur="({target})=>(target as HTMLInputElement).focus()"
+    @blur="({ target }) => (target as HTMLInputElement).focus()"
   />
 </template>
+
+<style scoped>
+input {
+  position: absolute;
+  opacity: 0;
+}
+.word {
+  list-style: none;
+  padding: 0;
+  display: flex;
+  gap: 0.25rem;
+}
+.letter {
+  background-color: white;
+  border: 1px solid hsl(0, 0%, 70%);
+  width: 5rem;
+  height: 5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 2rem;
+  font-weight: bolder;
+}
+li:not([data-letter=' ']) {
+  animation: pop 100ms;
+}
+@keyframes pop {
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.4);
+  }
+}
+</style>
